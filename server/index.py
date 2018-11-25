8401d004db0693bb5de2f3a2849b9df800ec3eaf
PineApple777 <metaljsw2@naver.com>
2018-11-25 16:22:48 +0000

    init folders

--- server/index.py
+++ server/index.py
@@ -1,10 +1,17 @@
-from flask import Flask
-app = Flask(__name__)
+import os
+from flask import Flask, render_template
+
+
+template_dir = os.path.abspath(os.path.join(
+    os.path.dirname(__file__), "../src/templates/"))
+print(template_dir)
+app = Flask(__name__, template_folder=template_dir)
 
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    print(template_dir)
+    return render_template('index.html')
 
 
 @app.route("/hello")
